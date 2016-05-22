41346d598007e446f31507bfc7575a4933211d37
Yohann Abittan <yohann.abittan@gmail.com>
2016-05-21 21:36:02 -0400

    Add files via upload

---
+++ CS1122 - hw01.py
@@ -0,0 +1,107 @@
+﻿import time
+import random
+
+'''
+programmer: Yohann Abittan
+netid:yaa243
+filename: hw01.py
+Aim: Testing out three different algorithms
+'''
+def insertionSort(firstList):
+   for index in range(1,len(firstList)):
+
+     currentvalue = firstList[index]
+     position = index
+
+     while position>0 and firstList[position-1]>currentvalue:
+         firstList[position]=firstList[position-1]
+         position = position-1
+
+     firstList[position]=currentvalue
+
+def quickSort(firstList):
+   quickSortHelper(firstList,0,len(firstList)-1)
+
+def quickSortHelper(firstList,first,last):
+   if first<last:
+
+       splitpoint = partition(firstList,first,last)
+
+       quickSortHelper(firstList,first,splitpoint-1)
+       quickSortHelper(firstList,splitpoint+1,last)
+
+
+def partition(firstList,first,last):
+   pivotvalue = firstList[first]
+
+   leftmark = first+1
+   rightmark = last
+
+   done = False
+   while not done:
+
+       while leftmark <= rightmark and firstList[leftmark] <= pivotvalue:
+           leftmark = leftmark + 1
+
+       while firstList[rightmark] >= pivotvalue and rightmark >= leftmark:
+           rightmark = rightmark -1
+
+       if rightmark < leftmark:
+           done = True
+       else:
+           temp = firstList[leftmark]
+           firstList[leftmark] = firstList[rightmark]
+           firstList[rightmark] = temp
+
+   temp = firstList[first]
+   firstList[first] = firstList[rightmark]
+   firstList[rightmark] = temp
+
+
+   return rightmark
+
+def selectionSort(firstList):
+   for fillslot in range(len(firstList)-1,0,-1):
+       positionOfMax=0
+       for location in range(1,fillslot+1):
+           if firstList[location]>firstList[positionOfMax]:
+               positionOfMax = location
+
+       temp = firstList[fillslot]
+       firstList[fillslot] = firstList[positionOfMax]
+       firstList[positionOfMax] = temp
+
+def main():
+	t0 = time.time()
+	firstList = range(1,1000,1)
+	random.shuffle(firstList)
+	insertionSort(firstList)
+	print(firstList)
+	t1 = time.time()
+	firstTotal = t1-t0
+
+	print(firstTotal)
+
+	t2 = time.time()
+ 	secondList = range(1,1000,1)
+ 	random.shuffle(secondList)
+ 	quickSort(secondList)
+ 	print(secondList)
+ 	t3 = time.time()
+ 	secondTotal = t3-t2
+
+ 	print(secondTotal)
+
+	t4 = time.time()
+	thirdList = range(1,1000,1)
+	random.shuffle(thirdList)
+	selectionSort(thirdList)
+	print(thirdList)
+	t5 = time.time()
+	thirdTotal = t5-t4
+
+	print(thirdTotal)
+
+
+if __name__ == "__main__":
+    main()
---
+++ CS1122 - hw02.py
@@ -0,0 +1,68 @@
+﻿'''
+fileName:CS1122hw2.py
+Programmer: Yohann Abittan
+netid: yaa243
+'''
+
+import string
+import urllib2
+import json
+import random
+
+
+from PIL import Image
+from PIL import ImageDraw
+from PIL import ImageFont
+
+def imageMaker():
+    nameOfUser = raw_input("what is your name?")
+    sizeOfImage = (240,270)
+    nameFont = ImageFont.truetype("Arial.ttf",20)
+    color = "#%06x" % random.randint(0,0xFFFFFF)
+    newImage = Image.new("RGB", sizeOfImage, (0,0,0))
+
+    coordinatesFace = (30,30,210,210)
+    coordinatesRightEye = (170,90,180,100)
+    coordinatesLeftEye = (60,90,70,100)
+    coordinatesMouth = [(110,130),(180,140)]
+    draw = ImageDraw.Draw(newImage)
+
+    draw.rectangle(coordinatesFace,color)
+
+    draw.ellipse(coordinatesRightEye,(250,250,250))
+
+    draw.ellipse(coordinatesLeftEye,(250,250,250))
+
+    draw.arc(coordinatesMouth, 10,170,(250,250,250))
+
+    draw.text((90,240),nameOfUser,(128,128,128),nameFont)
+
+    newImage.save("testImage","JPEG")
+    print("The image has been created")
+
+def readJsonFromURL():
+    opener = urllib2.build_opener()
+
+    request = urllib2.Request("http://elections.huffingtonpost.com/pollster/api/charts/2016-national-gop-primary")
+    
+    dataFile = opener.open(request)
+
+    jsonFile = json.loads(dataFile.read())
+
+    for index in jsonFile["estimates"]:
+
+        if index['first_name'] != None:
+
+            print(index['first_name'] + "  " + index['last_name'] + " : " + str(index['value']))
+        else:
+            print(index['choice'] + " : " + str(index['value']))
+
+def main():
+    imageMaker()
+    print("_____________")
+    readJsonFromURL()
+
+
+if __name__ == "__main__":
+    main()
+
